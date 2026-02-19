d44e8ea939b940dd463d0d0ec99701814e2e9453
kuykungg <zboybanz@gmail.com>
2026-02-19 16:13:22 +0700

    framework update

--- backend/src/routes/software.route.js
+++ backend/src/routes/software.route.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const SoftwareController = require("../controllers/SoftwareController")
 router.get('/',(req, res) =>{
     res.json({messge:"API work"});
 });
+router.post("/create",SoftwareController.createData);
 module.exports = router;
