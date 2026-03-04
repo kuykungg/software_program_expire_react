c9e9dbc390012d361e23a7c572d22b9a131484cb
kuykungg <zboybanz@gmail.com>
2026-03-04 15:45:00 +0700

    start notify service

---
+++ backend/src/database/migrations/20260304083415_create_notify_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("notify", (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+        table.uuid("psodftware_id");
+
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
