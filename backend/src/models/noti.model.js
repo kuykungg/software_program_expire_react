2651d5b11483eba2d1fe36d2828d6bbf7ebbf0d7
kuykungg <zboybanz@gmail.com>
2026-03-06 16:50:25 +0700

    cronjob (wip) broken

---
+++ backend/src/models/noti.model.js
@@ -0,0 +1,8 @@
+class Software{
+    constructor(data) {
+        this.id = data.id;
+        this.software_id = data.software_id;
+        this.details = data.details;
+    }
+}
+module.exports = noti;
