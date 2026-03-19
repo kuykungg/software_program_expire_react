8f088a2e845e27a5429ad006cb0697a6921dd295
kuykungg_officepc <zboybanz@gmail.com>
2026-03-19 15:58:00 +0700

    try websocket

--- notify_service/server.js
+++ notify_service/server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+//
 const app = express();
 const PORT = 3002;
+//
+const server = http.createServer(app);
+//
 const notifyrouter = require('./src/routes/notify.routes')
 const runCron = require('./src/cron/pull.data');
-runCron();
+//
+// 👇 create socket.io server
+const io = new Server(server, {
+    cors: {
+        origin: "*", // for dev, later restrict
+    }
+});
+
+// 👇 handle connection
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+    });
+});
+
+//
+runCron(io);
+app.set("io", io);
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use('/apiv1/notify',notifyrouter);
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
