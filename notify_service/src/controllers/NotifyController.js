a21f674c3fc965dca643c4fce549b9d88c2b6405
kuykungg_officepc <zboybanz@gmail.com>
2026-03-19 11:50:51 +0700

    crud

--- notify_service/src/controllers/NotifyController.js
+++ notify_service/src/controllers/NotifyController.js
@@ -5,4 +5,41 @@ exports.testapi = (req, res) =>{
     res.status(200).send(result);
 
 
+};
+exports.create = async(req,res) =>{
+    try{
+        console.log(req.body);
+        const result = await notifyservice.create(req.body);
+        res.status(201).json(result + "1");
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+        console.log(error);
     }
+};
+exports.update = async (req, res) => {
+    try {
+        console.log(req.body);
+        const result = await notifyservice.update(req.params.id, req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        console.log(error);
+    }
+};
+exports.delete = async (req, res) => {
+    try {
+        const result = await notifyservice.delete(req.params.id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+exports.read = async (req, res) => {
+    try {
+        const result = await notifyservice.read();
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- notify_service/src/routes/notify.routes.js
+++ notify_service/src/routes/notify.routes.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const notifycontroller = require("../controllers/NotifyController");
 router.get("/testapi",notifycontroller.testapi);
+router.get("/read",notifycontroller.read);
+router.post("/create",notifycontroller.create);
+router.put("/update/::id",notifycontroller.update);
+router.delete("delete/::id",notifycontroller.delete)
+
 module.exports = router;
