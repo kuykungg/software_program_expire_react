bec0705c3b2ae3b5525567bc4264e6f3176b37a4
kuykungg_officepc <zboybanz@gmail.com>
2026-03-19 11:53:43 +0700

    fix :: to :

--- notify_service/src/routes/notify.routes.js
+++ notify_service/src/routes/notify.routes.js
@@ -4,7 +4,7 @@ const notifycontroller = require("../controllers/NotifyController");
 router.get("/testapi",notifycontroller.testapi);
 router.get("/read",notifycontroller.read);
 router.post("/create",notifycontroller.create);
-router.put("/update/::id",notifycontroller.update);
-router.delete("delete/::id",notifycontroller.delete)
+router.put("/update/:id",notifycontroller.update);
+router.delete("delete/:id",notifycontroller.delete)
 
 module.exports = router;
